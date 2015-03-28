5c837d3777b82a900163d8d05b83aa85e2831d7a
Jarmo Kivekas <jarmo.kivekas@guttula.com>
2015-03-29 00:34:05 +0200

    add test for the dynamical model

---
+++ test/dynamical_model_test.c
@@ -0,0 +1,29 @@
+//dynamical_model_test.c
+#include <stdio.h>
+#include "../src/dynamical_model.h"
+
+
+int main(int argc, char const *argv[]) {
+    struct dynamic_design_t des = {0.0, 0.0};
+    struct dynamic_implement_t impl = {1.0, 1.0};
+    
+    
+    dynamic_implement_to_design(&impl, &des);
+    printf("straight forward:\ntranslation:\t%g\nrotation:\t%g\n\n", des.velo_translation, des.velo_rotation);
+
+    impl.velo_right = -1.0;
+    dynamic_implement_to_design(&impl, &des);
+    printf("o-turn clockwise:\ntranslation:\t%g\nrotation:\t%g\n\n", des.velo_translation, des.velo_rotation);
+
+    impl.velo_left =-1.0;
+    dynamic_implement_to_design(&impl, &des);
+    printf("straight backward:\ntranslation:\t%g\nrotation:\t%g\n\n", des.velo_translation, des.velo_rotation);
+
+    impl.velo_right = 0;
+    dynamic_implement_to_design(&impl, &des);
+    printf("u-turn a-clk backward:\ntranslation:\t%g\nrotation:\t%g\n\n", des.velo_translation, des.velo_rotation);
+
+
+
+    return 0;
+}

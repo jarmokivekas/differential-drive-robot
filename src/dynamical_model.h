3a81bd83f3c143b2f0364b6edf04413446aa755a
Jarmo Kivekas <jarmo.kivekas@guttula.com>
2015-03-29 00:13:48 +0200

    implement dynamical mathematical model

--- src/dynamical_model.h
+++ src/dynamical_model.h
@@ -4,3 +4,23 @@
 #define WHEEL_RADIUS 70.7
 #define WHEEL_SPACING 200.0
 
+
+// Used to describe the internal state of the robot's differential drive.
+// velo_X: rotational velocity of the corresponding wheel
+// values are measured in degrees per second.
+struct dynamic_implement_t {
+    float velo_right;
+    float velo_left;
+};
+
+// Used to describes higher-level commands regarding movement.
+// velo_tanslation: the robot's velo forward, meters per second
+// velo_rotation: the robot's rate of turning, degrees per second
+struct dynamic_design_t {
+    float velo_translation;
+    float velo_rotation;
+};
+
+
+void dynamic_design_to_implement(struct dynamic_design_t *design, struct dynamic_implement_t *implement);
+void dynamic_implement_to_design(struct dynamic_implement_t *implement, struct dynamic_design_t *design);
